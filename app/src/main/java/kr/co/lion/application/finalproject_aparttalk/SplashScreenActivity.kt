b675aa2fd63f70f96554b27d09c9d3e7e5749d70
seongukHeo <heoseonguk3@gmail.com>
2024-05-19 19:31:40 +0900

    feat : #3 SplashScreen 구현 완료

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/SplashScreenActivity.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/SplashScreenActivity.kt
@@ -1,20 +1,27 @@
 package kr.co.lion.application.finalproject_aparttalk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        showSplashScreen()
+    }
+
+    private fun showSplashScreen(){
+        lifecycleScope.launch {
+            delay(2000)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
         }
     }
 }
