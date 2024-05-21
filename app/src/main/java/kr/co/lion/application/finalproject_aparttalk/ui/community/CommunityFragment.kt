6071749b3cbe74a28432bff53f65899cf2e3bce2
hyuun <wotj28@naver.com>
2024-05-21 11:21:41 +0900

    [feat] #13 CommunityFragment.kt UI 완성

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/community/CommunityFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/community/CommunityFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.co.lion.application.finalproject_aparttalk.R
+import kr.co.lion.application.finalproject_aparttalk.MainActivity
+
 import kr.co.lion.application.finalproject_aparttalk.databinding.FragmentCommunityBinding
 
 class CommunityFragment : Fragment() {
 
     lateinit var binding:FragmentCommunityBinding
+    lateinit var mainActivity: MainActivity
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        binding = FragmentCommunityBinding.inflate(layoutInflater)
+        binding = FragmentCommunityBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
         return binding.root
     }
 }
