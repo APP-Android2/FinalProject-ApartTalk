56322544060903852a2b8d12be2ae2f876bb0b51
cny1213 <cny1213@gmail.com>
2024-05-27 04:16:39 +0900

    #23 feat 소방점검 화면 구현

---
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/entiremenu/AptOperationInfo/OperationInfoActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.AptOperationInfo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.co.lion.application.finalproject_aparttalk.R
+
+class OperationInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_operation_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
