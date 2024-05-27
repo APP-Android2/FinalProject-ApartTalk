7905458259b17be7a05e4cb5907a66255a5ebf8c
iamByeongJin <leej4528@naver.com>
2024-05-27 09:16:15 +0900

    #9 Feat Login 화면 추가

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/login/LoginFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/login/LoginFragment.kt
@@ -1,11 +1,12 @@
 package kr.co.lion.application.finalproject_aparttalk.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kr.co.lion.application.finalproject_aparttalk.LoginActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kr.co.lion.application.finalproject_aparttalk.R
 import kr.co.lion.application.finalproject_aparttalk.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -15,15 +16,34 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentLoginBinding.inflate(inflater)
-
-        login()
-
         return binding.root
     }
 
-    private fun login(){
-        binding.buttonLogin.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        socialLoginButton()
+        phoneLoginButton()
+    }
+
+    private fun socialLoginButton(){
+
+        binding.googleLoginButton.setOnClickListener {
             (requireActivity() as LoginActivity).navigateToMain()
+        }
+
+        binding.kakaoLoginButton.setOnClickListener {
+            (requireActivity() as LoginActivity).navigateToMain()
+        }
+
+        binding.naverLoginButton.setOnClickListener {
+            (requireActivity() as LoginActivity).navigateToMain()
+        }
+    }
+
+    private fun phoneLoginButton() {
+        binding.phoneNumberLoginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_login2Fragment)
         }
     }
 
