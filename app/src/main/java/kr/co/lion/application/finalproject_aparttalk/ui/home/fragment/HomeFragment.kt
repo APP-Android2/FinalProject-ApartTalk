cbc36da6860b44192b52258363d36ba4dd79379d
iamByeongJin <leej4528@naver.com>
2024-05-29 16:39:44 +0900

    #94 [feat] 주변 시설 이동 기능 추가

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
@@ -85,16 +85,24 @@ class HomeFragment : Fragment() {
             }
 
             cardViewRestaurant.setOnClickListener {
-
+                val bundle = Bundle()
+                bundle.putInt("tabPosition", 1) // 음식점
+                mainActivity.replaceFragment(MainFragmentName.LOCATION_FRAGMENT, true, bundle)
             }
             cardViewHospital.setOnClickListener {
-
+                val bundle = Bundle()
+                bundle.putInt("tabPosition", 2) // 병원
+                mainActivity.replaceFragment(MainFragmentName.LOCATION_FRAGMENT, true, bundle)
             }
             cardViewPharmacy.setOnClickListener {
-
+                val bundle = Bundle()
+                bundle.putInt("tabPosition", 3) // 약국
+                mainActivity.replaceFragment(MainFragmentName.LOCATION_FRAGMENT, true, bundle)
             }
             cardViewCafe.setOnClickListener {
-
+                val bundle = Bundle()
+                bundle.putInt("tabPosition", 4) // 카페
+                mainActivity.replaceFragment(MainFragmentName.LOCATION_FRAGMENT, true, bundle)
             }
         }
     }
