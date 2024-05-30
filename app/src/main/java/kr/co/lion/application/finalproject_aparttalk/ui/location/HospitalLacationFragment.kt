a057a250a2bde178b4652a448ae1c0f624a031d3
seongukHeo <heoseonguk3@gmail.com>
2024-05-31 00:20:53 +0900

    feat : #108 카테고리 데이터 불러오기 구현(병원)

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/location/HospitalLacationFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/location/HospitalLacationFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import kotlinx.coroutines.launch
 import kr.co.lion.application.finalproject_aparttalk.R
 import kr.co.lion.application.finalproject_aparttalk.databinding.FragmentHospitalLacationBinding
 import kr.co.lion.application.finalproject_aparttalk.model.LocationExtraData
 import kr.co.lion.application.finalproject_aparttalk.ui.location.adapter.ExtraAdapter
+import kr.co.lion.application.finalproject_aparttalk.ui.location.viewmodel.EachLocationViewModel
 
 class HospitalLacationFragment : Fragment() {
 
     lateinit var binding: FragmentHospitalLacationBinding
+
+    val viewModel : EachLocationViewModel by viewModels()
 
     val extraAdapter : ExtraAdapter by lazy {
         val adapter = ExtraAdapter()
@@ -34,6 +41,7 @@ class HospitalLacationFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHospitalLacationBinding.inflate(layoutInflater)
         settingRecyclerview()
+        gettingData()
         return binding.root
     }
 
@@ -46,6 +54,17 @@ class HospitalLacationFragment : Fragment() {
                 addItemDecoration(deco)
 
             }
+        }
+    }
+
+    //데이터 받아오기
+    private fun gettingData(){
+        viewModel.eachLocationList.observe(viewLifecycleOwner, Observer { places ->
+            extraAdapter.submitList(places)
+        })
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.searchEachLocationPlace("HP8", "127.05897078335246", "37.506051888130386", 2000)
         }
     }
 }
