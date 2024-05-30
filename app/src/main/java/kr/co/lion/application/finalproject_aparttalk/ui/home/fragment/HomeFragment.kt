3ecb9285621b642f03341a4551876335b59e270d
seongukHeo <heoseonguk3@gmail.com>
2024-05-30 09:11:43 +0900

    fix : #91 일부 코드 수정

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
@@ -18,6 +18,7 @@ import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.AptSchedule.A
 import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.FireCheck.FireCheckActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.home.adapter.HomeNotificationRecyclerView
 import kr.co.lion.application.finalproject_aparttalk.ui.info.InfoActivity
+import kr.co.lion.application.finalproject_aparttalk.ui.parking.ParkingActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.vote.VoteActivity
 import kr.co.lion.application.finalproject_aparttalk.util.BroadcastFragmentName
 import kr.co.lion.application.finalproject_aparttalk.util.MainFragmentName
@@ -57,6 +58,7 @@ class HomeFragment : Fragment() {
             }
             homeAllVisitCarLayout.setOnClickListener {
                 //방문 주차 예약
+                startActivity(Intent(requireActivity(), ParkingActivity::class.java))
             }
             homeAllVoteLayout.setOnClickListener {
                 //주민 투표
