35951f4987779bdee55e964ee616077cf20f2dbf
seongukHeo <heoseonguk3@gmail.com>
2024-05-31 00:19:51 +0900

    feat : #108 카테고리 데이터 불러오기 구현(음식점)

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/location/FoodLocationFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/location/FoodLocationFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import kotlinx.coroutines.launch
 import kr.co.lion.application.finalproject_aparttalk.R
 import kr.co.lion.application.finalproject_aparttalk.databinding.FragmentFoodLocationBinding
 import kr.co.lion.application.finalproject_aparttalk.model.LocationExtraData
 import kr.co.lion.application.finalproject_aparttalk.ui.location.adapter.ExtraAdapter
+import kr.co.lion.application.finalproject_aparttalk.ui.location.viewmodel.EachLocationViewModel
 
 class FoodLocationFragment : Fragment() {
 
     lateinit var binding: FragmentFoodLocationBinding
+
+    val viewModel : EachLocationViewModel by viewModels()
 
     val extraAdapter : ExtraAdapter by lazy {
         val adapter = ExtraAdapter()
@@ -35,6 +42,7 @@ class FoodLocationFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentFoodLocationBinding.inflate(layoutInflater)
         settingRecyclerview()
+        gettingData()
         return binding.root
     }
 
@@ -47,6 +55,17 @@ class FoodLocationFragment : Fragment() {
                 addItemDecoration(deco)
 
             }
+        }
+    }
+
+    //데이터 받아오기
+    private fun gettingData(){
+        viewModel.eachLocationList.observe(viewLifecycleOwner, Observer { places ->
+            extraAdapter.submitList(places)
+        })
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.searchEachLocationPlace("FD6", "127.05897078335246", "37.506051888130386", 2000)
         }
     }
 }
