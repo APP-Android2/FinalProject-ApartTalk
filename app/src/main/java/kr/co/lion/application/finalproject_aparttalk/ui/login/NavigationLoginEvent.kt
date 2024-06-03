49117be3369d9c6970b6e31e1c937a0df083fd55
iamByeongJin <leej4528@naver.com>
2024-06-03 16:55:52 +0900

    #104 [Remove] NavigationLoginEvent.kt 불필요 파일 삭제

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/login/NavigationLoginEvent.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.lion.application.finalproject_aparttalk.ui.login
-
-sealed class NavigationLoginEvent {
-    data object NavigationToMain: NavigationLoginEvent()
-    data object NavigationToSignUp: NavigationLoginEvent()
-    data object NavigationToLogin: NavigationLoginEvent()
-}
