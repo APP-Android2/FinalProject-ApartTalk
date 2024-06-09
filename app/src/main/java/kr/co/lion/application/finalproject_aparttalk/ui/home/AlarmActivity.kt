ee3a1c6f0cb2f6c4be22070a3f9d18215c3dc2ce
seongukHEO <heoseonguk3@gmail.com>
2024-06-09 15:12:36 +0900

    feat : #170 알림 Adapter 연결

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/AlarmActivity.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/AlarmActivity.kt
@@ -1,22 +1,32 @@
 package kr.co.lion.application.finalproject_aparttalk.ui.home
 
 import android.os.Bundle
+import android.widget.Adapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import kr.co.lion.application.finalproject_aparttalk.R
 import kr.co.lion.application.finalproject_aparttalk.databinding.ActivityAlarmBinding
 import kr.co.lion.application.finalproject_aparttalk.databinding.ActivityMainBinding
+import kr.co.lion.application.finalproject_aparttalk.ui.home.adapter.AlarmAdapter
 
 class AlarmActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityAlarmBinding
+
+    val alarmAdapter : AlarmAdapter by lazy {
+        val adapter = AlarmAdapter()
+        adapter
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
         settingToolbar()
+        connectAdapter()
     }
 
     //툴바 설정
@@ -27,6 +37,18 @@ class AlarmActivity : AppCompatActivity() {
                 setNavigationOnClickListener {
                     finish()
                 }
+            }
+        }
+    }
+
+    //어댑터 연결
+    private fun connectAdapter(){
+        binding.apply {
+            recyclerviewAlarm.apply {
+                adapter = alarmAdapter
+                layoutManager = LinearLayoutManager(this@AlarmActivity)
+                val deco = MaterialDividerItemDecoration(this@AlarmActivity, MaterialDividerItemDecoration.VERTICAL)
+                addItemDecoration(deco)
             }
         }
     }
