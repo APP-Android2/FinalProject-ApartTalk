1f0b2e25ef7c8265411d300c7d5c120151148e60
seongukHEO <heoseonguk3@gmail.com>
2024-06-10 11:09:37 +0900

    fix : #181 코드 및 레이아웃 수정

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/entiremenu/EntireMenuFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/entiremenu/EntireMenuFragment.kt
@@ -81,11 +81,20 @@ class EntireMenuFragment : Fragment() {
                 if (authUser != null){
                     val user = App.userRepository.getUser(authUser.uid)
                     if (user != null){
+
+                        mypageMenu.setOnClickListener {
+                            //마이페이지로 이동
+                            startActivity(Intent(requireActivity(), InfoActivity::class.java))
+                        }
+
+                        buttonUserLogout.setOnClickListener {
+                            // 로그인 화면으로 이동
+                            App.authRepository.signOut()
+                            startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                            requireActivity().finish()
+                        }
+
                         if (user.apartCertification == true){
-                            mypageMenu.setOnClickListener {
-                                //마이페이지로 이동
-                                startActivity(Intent(requireActivity(), InfoActivity::class.java))
-                            }
 
                             LinearWriteMenu.setOnClickListener {
                                 //내가 쓴 글로 이동
@@ -132,12 +141,6 @@ class EntireMenuFragment : Fragment() {
                             buttonAptInfoMenu.setOnClickListener {
                                 //아파트 운영정보로 이동
                                 startActivity(Intent(requireActivity(), OperationInfoActivity::class.java))
-                            }
-                            buttonUserLogout.setOnClickListener {
-                                // 로그인 화면으로 이동
-                                App.authRepository.signOut()
-                                startActivity(Intent(requireActivity(), LoginActivity::class.java))
-                                requireActivity().finish()
                             }
                         }else{
                             LinearWriteMenu.isClickable = false
