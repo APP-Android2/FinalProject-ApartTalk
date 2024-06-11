8acd113dcf7834918ef1890e806ab8b567369b98
cny1213 <cny1213@gmail.com>
2024-06-11 15:19:10 +0900

    #182 [fix] 아파트일정 홈화면에서 캘린더 커스텀 대신 클릭시 리사이클러 띄움

--- app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/kr/co/lion/application/finalproject_aparttalk/ui/home/fragment/HomeFragment.kt
@@ -5,18 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kr.co.lion.application.finalproject_aparttalk.App
 import kr.co.lion.application.finalproject_aparttalk.MainActivity
 import kr.co.lion.application.finalproject_aparttalk.databinding.FragmentHomeBinding
 import kr.co.lion.application.finalproject_aparttalk.ui.broadcast.activity.BroadcastActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.AptSchedule.AptScheduleActivity
+import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.AptSchedule.AptScheduleRecyclerView
+import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.AptSchedule.AptScheduleViewModel
 import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.FireCheck.FireCheckActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.entiremenu.OperationInfo.OperationInfoActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.home.AlarmActivity
+import kr.co.lion.application.finalproject_aparttalk.ui.home.adapter.HomeAptScheduleRecyclerView
 import kr.co.lion.application.finalproject_aparttalk.ui.home.adapter.HomeNotificationRecyclerView
 import kr.co.lion.application.finalproject_aparttalk.ui.info.InfoActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.inquiry.InquiryActivity
@@ -24,11 +32,18 @@ import kr.co.lion.application.finalproject_aparttalk.ui.parking.ParkingActivity
 import kr.co.lion.application.finalproject_aparttalk.ui.vote.VoteActivity
 import kr.co.lion.application.finalproject_aparttalk.util.BroadcastFragmentName
 import kr.co.lion.application.finalproject_aparttalk.util.MainFragmentName
+import java.util.Calendar
 
 class HomeFragment : Fragment() {
 
     lateinit var binding:FragmentHomeBinding
     lateinit var mainActivity: MainActivity
+
+    private val aptViewModel: AptScheduleViewModel by viewModels()
+    private lateinit var homeAptScheduleRecyclerView: HomeAptScheduleRecyclerView
+
+    // 선택된 날짜를 저장할 변수
+    private var selectedDate: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -39,6 +54,12 @@ class HomeFragment : Fragment() {
         settingRecyclerViewHomeNotification()
         settingEvent()
         initView()
+        setRecyclerAptSchedule()
+
+        // 코루틴을 사용하여 gettingApartId 호출 및 setCalendarView 설정
+        GlobalScope.launch(Dispatchers.Main) {
+            setCalendarView(gettingApartId())
+        }
 
         return binding.root
     }
@@ -136,6 +157,73 @@ class HomeFragment : Fragment() {
                 bundle.putInt("tabPosition", 4) // 카페
                 mainActivity.replaceFragment(MainFragmentName.LOCATION_FRAGMENT, false, bundle)
             }
+        }
+    }
+
+    // 아파트 아이디 가져오기
+    suspend fun gettingApartId(): String {
+        var apartmentId = ""
+        try {
+            val authUser = App.authRepository.getCurrentUser()
+            if (authUser != null) {
+                val user = App.userRepository.getUser(authUser.uid)
+                if (user != null) {
+                    val apartment = App.apartmentRepository.getApartment(user.apartmentUid)
+                    apartmentId = apartment!!.uid
+                }
+            }
+        } catch (e: Exception) {
+            //Log.e("BiddingNoticeFragment", "Error fetching apartment ID", e)
+        }
+        //Log.d("BiddingNoticeFragment", "Apartment ID: $apartmentId")
+        return apartmentId
+    }
+
+    // 캘린더뷰 설정
+    private fun setCalendarView(apartmentUid: String) {
+
+        // 현재 날짜를 "yyyy-MM-dd" 형식의 문자열로 저장합니다.
+        val calendar = Calendar.getInstance()
+        selectedDate = String.format("%04d-%02d-%02d", calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH))
+
+        getSelectedDateDate(apartmentUid)
+
+        binding.homeCalendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // 사용자가 선택한 날짜를 "yyyy-MM-dd" 형식의 문자열로 저장합니다.
+            selectedDate = String.format("%04d-%02d-%02d", year, month + 1, dayOfMonth)
+
+            getSelectedDateDate(apartmentUid)
+
+            // ScheduleType을 숨기고 ScheduleList를 표시합니다.
+            binding.linearLayoutScheduleType.visibility = View.GONE
+            binding.linearLayoutScheduleList.visibility = View.VISIBLE
+        }
+    }
+
+    // 해당 날짜의 일정을 가져옵니다.
+    fun getSelectedDateDate(apartmentUid: String){
+        // selectedDate가 null이 아닌 경우에만 ViewModel을 통해 해당 날짜의 일정을 가져옵니다.
+        selectedDate?.let { date ->
+            // 코루틴을 사용하여 gettingSelectedDateList를 호출합니다.
+            GlobalScope.launch(Dispatchers.Main) {
+                val selectedDateList = aptViewModel.gettingSelectedDateList(apartmentUid, date)
+                // 선택된 날짜의 일정 목록을 어댑터에 설정합니다.
+                homeAptScheduleRecyclerView.setScheduleList(selectedDateList)
+            }
+        }
+    }
+
+    // recyclerView 설정
+    fun setRecyclerAptSchedule() {
+        homeAptScheduleRecyclerView = HomeAptScheduleRecyclerView()
+        binding.recyclerViewAptScheduleList.apply {
+            // 어댑터 설정
+            adapter = homeAptScheduleRecyclerView
+            // 레이아웃 매니저 설정
+            layoutManager = LinearLayoutManager(requireContext())
+            // 데코
+            val deco = MaterialDividerItemDecoration(requireContext(), MaterialDividerItemDecoration.VERTICAL)
+            addItemDecoration(deco)
         }
     }
 
